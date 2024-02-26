634370eff18984a7b9784dce90794cbbace09eb3
ΓΙΩΡΓΟΣ ΠΑΠΑΓΕΩΡΓΙΟΥ <papageorgiou.90@gmail.com>
2024-02-26 16:44:35 +0200

    ConfigureServicesDI

---
+++ ConfigureServicesDI/ConfigureServicesDI/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ConfigureServicesDI
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            IHost host = Host.CreateDefaultBuilder().ConfigureServices(services =>
+            {
+                services.AddSingleton<IApplication, Application>();
+                services.AddSingleton<IDummy, Dummy>();
+            }).Build();
+
+            var app = host.Services.GetRequiredService<IDummy>();
+            var app2 = host.Services.GetRequiredService<IApplication>();
+            app.DoSomething();
+            app2.Run();
+
+        }
+    }
+}

bf1f835ff1544894b96d2a168926e24bd193f270
ΓΙΩΡΓΟΣ ΠΑΠΑΓΕΩΡΓΙΟΥ <papageorgiou.90@gmail.com>
2024-02-28 15:17:54 +0200

    Data Adapter Project

---
+++ ADO.NET Introduction/DataAdapter/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+
+namespace DataAdapterUsingDataTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string connectionString = "data source=DESKTOP; database=Northwind; integrated security = SSPI";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    SqlDataAdapter dataAdapter = new SqlDataAdapter("select * from customers", connection);
+                    DataTable dataTable = new DataTable();
+                    dataAdapter.Fill(dataTable);
+
+                    foreach(DataRow row in  dataTable.Rows)
+                    {
+                        Console.WriteLine(row["CompanyName"] + " " + row["Phone"]);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("something went wrong");
+            }
+        }
+    }
+}

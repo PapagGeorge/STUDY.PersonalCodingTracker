dd0614f180cb57a0c7cc845e6be3281ff6a47c0b
ΓΙΩΡΓΟΣ ΠΑΠΑΓΕΩΡΓΙΟΥ <papageorgiou.90@gmail.com>
2024-02-28 14:40:59 +0200

    Created DataReader project

---
+++ ADO.NET Introduction/ADO.NET Introduction/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Data.SqlClient;
+
+namespace ADO.NET_Introduction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string conString = "data source=DESKTOP; database=Northwind; integrated security =SSPI";
+
+                using (SqlConnection connection = new SqlConnection(conString))
+                {
+                    SqlCommand cmd = new SqlCommand("select * from Customers", connection);
+                    connection.Open();
+                    SqlDataReader sdr = cmd.ExecuteReader();
+
+                    while (sdr.Read())
+                    {
+                        Console.WriteLine(sdr["CompanyName"] + " " + sdr["City"] + " " + sdr["Phone"]);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Something went wrong");
+            }
+            Console.ReadKey();
+        }
+    }
+}
--- DI Practise/DI Practise/Program.cs
+++ DI Practise/DI Practise/Program.cs
@@ -12,13 +12,11 @@ namespace DI_Practise
         static void Main(string[] args)
         {
             var host = Host.CreateDefaultBuilder().ConfigureServices(services =>
-            {
-                services.AddTransient<IApplication2, Application2>();
-                services.AddTransient<IApplication, Application>();
-                services.AddTransient<IClassFromAnotherProject, ClassFromAnotherProject>();
-                services.GetServices();
-
-
+            { services.AddTransient<IApplication2, Application2>();
+              services.AddTransient<IApplication, Application>();
+              services.AddTransient<IClassFromAnotherProject, ClassFromAnotherProject>();
+              services.GetServices();
+            
             }).Build();
 
             var app = host.Services.GetRequiredService<IApplication>();

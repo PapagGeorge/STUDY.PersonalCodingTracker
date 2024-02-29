79ea594b7defa9d0c0fa3f6796076b19f75ca021
ΓΙΩΡΓΟΣ ΠΑΠΑΓΕΩΡΓΙΟΥ <papageorgiou.90@gmail.com>
2024-02-29 17:59:57 +0200

    Stored Procedure with input parameter

---
+++ ADO.NET Introduction/StoredProcedureWithInputParameters/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+
+namespace StoredProcedureWithInputParameters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string connectionString = "data source=DESKTOP; database=StudentDb; integrated security=SSPI";
+                using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+                {
+                    SqlCommand sqlCommand = new SqlCommand("spGetStudentsById", sqlConnection);
+                    sqlCommand.CommandType = CommandType.StoredProcedure;
+
+                    SqlParameter param1 = new SqlParameter
+                    {
+                        ParameterName = "@Id",
+                        SqlDbType = SqlDbType.Int,
+                        Value = 100,
+                        Direction = ParameterDirection.Input
+                    };
+
+                    sqlCommand.Parameters.Add(param1);
+                    sqlConnection.Open();
+                    SqlDataReader dataReader = sqlCommand.ExecuteReader();
+
+                    while (dataReader.Read())
+                    {
+                        Console.WriteLine(dataReader["Id"] + " " + dataReader["Name"] + " " + dataReader["Email"] + " " + dataReader["Mobile"]);
+                    }
+                }
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+         }
+            
+                    
+    }
+}

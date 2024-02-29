45248a8753872c839ab2161368350e829b98c040
ΓΙΩΡΓΟΣ ΠΑΠΑΓΕΩΡΓΙΟΥ <papageorgiou.90@gmail.com>
2024-02-29 17:11:02 +0200

    Call Stored Procedure without parameters

---
+++ ADO.NET Introduction/StoredProcedureWithoutParameters/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace StoredProcedureWithoutParameters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string connectionString = "data source=DESKTOP; database=StudentDb; integrated security=SSPI";
+                using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+                {
+                    SqlCommand cmd = new SqlCommand("SpGetStudents", sqlConnection);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    sqlConnection.Open();
+                    SqlDataReader dataReader = cmd.ExecuteReader();
+
+                    while (dataReader.Read())
+                    {
+                        Console.WriteLine(dataReader["Name"] + " " + dataReader["Mobile"]);
+                    }
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ ConfigurationFile/ConfigurationFile/Address.cs
@@ -0,0 +1,22 @@
+﻿using System.Configuration;
+
+namespace ConfigurationFile
+{
+    public class Address : ConfigurationElement
+    {
+        [ConfigurationProperty ("StreetNumber")]
+        public int StreetNumber
+        {
+            get => Convert.ToInt32(this["StreetNumber"].ToString());
+            set => this["StreetNumber"] = value;
+        }
+
+        [ConfigurationProperty ("RoomNumber")]
+        public int RoomNumber
+        {
+            get => Convert.ToInt32(this["RoomNumber"].ToString());
+            set => this["RoomNumber"] = value;
+        }
+
+    }
+}

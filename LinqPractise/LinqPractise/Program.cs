43fb04ded9428189a60acff9fd604061694a58ab
ΓΙΩΡΓΟΣ ΠΑΠΑΓΕΩΡΓΙΟΥ <papageorgiou.90@gmail.com>
2024-03-11 16:39:41 +0200

    Any Operation

--- LinqPractise/LinqPractise/Program.cs
+++ LinqPractise/LinqPractise/Program.cs
@@ -291,20 +291,60 @@ namespace LinqPractise
 
 
 
-            Console.WriteLine("----------GroupBy using Query Syntax----------");
+            //Console.WriteLine("----------GroupBy using Query Syntax----------");
+            //List<Employee> employeeList = Data.GetEmployees();
+            //List<Department> departmentList = Data.GetDepartments();
+
+            //var results = from employee in employeeList
+            //              group employee by employee.DepartmentId;
+
+            //foreach(var empGroup in results)
+            //{
+            //    Console.WriteLine($"Department Id: {empGroup.Key}");
+            //    foreach(Employee emp in empGroup)
+            //    {
+            //Console.WriteLine($"Full Name: {emp.FirstName} {emp.LastName}, Annual Salary: {emp.AnnualSalary}, Department Id: {emp.DepartmentId}");
+            //    }
+            //}
+
+
+
+
+
+            //Console.WriteLine("----------ToLookUp using Method Syntax----------");
+            //List<Employee> employeeList = Data.GetEmployees();
+            //List<Department> departmentList = Data.GetDepartments();
+
+            //var results = employeeList.OrderBy(x => x.DepartmentId).ToLookup(employee => employee.DepartmentId);
+            //foreach (var groupEmp in results)
+            //{
+            //    Console.WriteLine($"Department Id: {groupEmp.Key}");
+            //    foreach(var emp in groupEmp)
+            //    {
+            //        Console.WriteLine($"Full Name: {emp.FirstName} {emp.LastName}, Annual Salary: {emp.AnnualSalary}, Department Id: {emp.DepartmentId}");
+            //    }
+            //}
+
+
+
+
+            
+
+            Console.WriteLine("----------All function Method Syntax----------");
+
             List<Employee> employeeList = Data.GetEmployees();
             List<Department> departmentList = Data.GetDepartments();
-
-            var results = from employee in employeeList
-                          group employee by employee.DepartmentId;
             
-            foreach(var empGroup in results)
+            var annualSalaryCompare = 2000;
+            bool isTrueAll =  employeeList.All(emp => emp.AnnualSalary >  annualSalaryCompare);
+
+            if(isTrueAll )
             {
-                Console.WriteLine($"Department Id: {empGroup.Key}");
-                foreach(Employee emp in empGroup)
-                {
-                    Console.WriteLine($"Full Name: {emp.FirstName} {emp.LastName}, Annual Salary: {emp.AnnualSalary}, Department Id: {emp.DepartmentId}");
-                }
+                Console.WriteLine("All employee salaries exceed 2000");
+            }
+            else
+            {
+                Console.WriteLine("Not all employee salaries exceed 2000");
             }
         }
 

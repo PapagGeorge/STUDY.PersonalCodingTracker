99fac74bf9bd13993e2133d442fdd5dd48648dd3
ΓΙΩΡΓΟΣ ΠΑΠΑΓΕΩΡΓΙΟΥ <papageorgiou.90@gmail.com>
2024-03-13 15:21:29 +0200

    Domain Classes

---
+++ EntityFrameworkCoreIntroduction/EntityFrameworkCoreIntroduction/Models/Manager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCoreIntroduction.Models
+{
+    public class Manager
+    {
+        public int ManagerId { get; set; }
+        public string MngFirstName { get; set; }
+        public string MngLastName { get; set; }
+    }
+}

8b4e0338a177c7d9c672155a4d802cda6b548393
ΓΙΩΡΓΟΣ ΠΑΠΑΓΕΩΡΓΙΟΥ <papageorgiou.90@gmail.com>
2024-03-13 15:32:07 +0200

    DbContext Class

---
+++ EntityFrameworkCoreIntroduction/EntityFrameworkCoreIntroduction/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkCoreIntroduction.Data
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+    }
+}

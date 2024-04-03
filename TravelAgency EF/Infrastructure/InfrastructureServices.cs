9ce31ee36bfb52d47a6839a2a0f6f467d17ceeee
ΓΙΩΡΓΟΣ ΠΑΠΑΓΕΩΡΓΙΟΥ <papageorgiou.90@gmail.com>
2024-04-03 20:55:03 +0300

    Set InfraServices

---
+++ TravelAgency EF/Infrastructure/InfrastructureServices.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Application.Interfaces;
+using System.Configuration;
+using Infrastructure.Repositories;
+
+namespace Infrastructure
+{
+    public static class InfrastructureServices
+    {
+        public static IServiceCollection InfraServices(IServiceCollection services)
+        {
+            services.AddSingleton<IAnalyticsRepository, AnalyticsRepository>();
+            services.AddSingleton<ICustomerRepository, CustomerRepository>();
+            services.AddSingleton<ITripRepository, TripRepository>();
+
+            return services;
+        }
+    }
+}

fe1775ce61fa7b7a3d5e94f0c51a9e4eee3c19e6
ΓΙΩΡΓΟΣ ΠΑΠΑΓΕΩΡΓΙΟΥ <papageorgiou.90@gmail.com>
2024-04-05 17:49:24 +0300

    Register Apllication Services

---
+++ TravelAgency EF/Application/ApplicationModule.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Application.Interfaces;
+using Application.Services;
+
+namespace Application
+{
+    public static class ApplicationModule
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IApplication, Services.Application>();
+            return services;
+        }
+    }
+}

fbfa03b7aa4811623883492dfda8e88b8922d811
ΓΙΩΡΓΟΣ ΠΑΠΑΓΕΩΡΓΙΟΥ <papageorgiou.90@gmail.com>
2024-04-12 18:47:41 +0300

    OrderService

--- SalesWebApi/Infrastructure/Repositories/OrderService.cs
+++ SalesWebApi/Infrastructure/Repositories/OrderService.cs
@@ -5,9 +5,28 @@ namespace Infrastructure.Repositories
 {
     public class OrderService : IOrderService
     {
-        public Order CreateOrder(int customerId, int paymentMethodId)
+
+        private readonly SalesDbContext _context;
+
+        public OrderService(SalesDbContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
+        }
+        public void CreateOrder(int customerId, int paymentMethodId)
+        {
+            try
+            {
+                var order = new Order();
+                order.customerId = customerId;
+                order.PaymentMethodId = paymentMethodId;
+                _context.Orders.Add(order);
+                _context.SaveChanges();
+                
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"An error occured while creating new order. {ex.Message}");
+            }
         }
     }
 }

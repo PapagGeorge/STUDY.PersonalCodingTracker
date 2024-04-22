26999d35cfaea1904df6baa3c436fc600f125849
ΓΙΩΡΓΟΣ ΠΑΠΑΓΕΩΡΓΙΟΥ <papageorgiou.90@gmail.com>
2024-04-22 16:22:20 +0300

    Users Controller

---
+++ BetAgencyUsingWebApi/WebApi/Controllers/UsersController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Application.Interfaces;
+using Domain.Entities;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IApplication _application;
+
+        public UsersController(IApplication application)
+        {
+            _application = application;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllUsers()
+        {
+            try
+            {
+                var users = _application.GetAllUsers().ToList();
+
+                if (users.Count == 0)
+                {
+                    return NotFound("No users were found");
+                }
+                return Ok(users);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetUserById(int id)
+        {
+            try
+            {
+                var user = _application.GetUserById(id);
+
+                if(user == null)
+                {
+                    return NotFound($"User with Id: {id} not found.");
+                }
+
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPost("add-user")]
+        public IActionResult AddUser([FromBody] User user)
+        {
+            try
+            {
+                if (user == null)
+                {
+                    return BadRequest("User object is null.");
+                }
+
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest("User object doesn't provide all necessary information");
+                }
+
+                _application.CreateUser(user);
+
+                return CreatedAtAction(nameof(GetUserById), new { id = user.UserId }, user);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}

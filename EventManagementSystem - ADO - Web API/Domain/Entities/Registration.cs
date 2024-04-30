7516cd62833edcb56d37ad2ffdc7459f4188319f
ΓΙΩΡΓΟΣ ΠΑΠΑΓΕΩΡΓΙΟΥ <papageorgiou.90@gmail.com>
2024-04-30 19:21:44 +0300

    Created Domain Entities

---
+++ EventManagementSystem - ADO - Web API/Domain/Entities/Registration.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Entities
+{
+    public class Registration
+    {
+        public int RegistrationId { get; set; }
+        public int EventId { get; set; }
+        public int UserId { get; set; }
+        public DateTime RegistrationDatTime { get; set; }
+        public string Status { get; set; }
+    }
+}

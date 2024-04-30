56619b3d4218e42bef88a9135ce354f24c828ae9
ΓΙΩΡΓΟΣ ΠΑΠΑΓΕΩΡΓΙΟΥ <papageorgiou.90@gmail.com>
2024-04-30 21:16:04 +0300

    IAttendance,Repository, IEventERepository, IRegistrationRepository, IUserRepository Interfaces set

---
+++ EventManagementSystem - ADO - Web API/Application/Interfaces/IEventRepository.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Entities;
+
+namespace Application.Interfaces
+{
+    public interface IEventRepository
+    {
+        IEnumerable<Event> EventsInDateRange (DateTime start, DateTime end);
+    }
+}
--- EventManagementSystem - ADO - Web API/Application/Interfaces/IGenericRepository.cs
+++ EventManagementSystem - ADO - Web API/Application/Interfaces/IGenericRepository.cs
@@ -5,8 +5,8 @@
         IEnumerable<TEntity> GetAll<TEntity>();
         TEntity GetById<TEntity> (int id);
         void Insert<TEntity> (TEntity entity);
+        void BulkInsert<TEntity>(IEnumerable<TEntity> entities);
         void Update<TEntity> (TEntity entity);
-        void BulkUpdate<TEntity> (IEnumerable<TEntity> entities);
         void Delete<TEntity> (int id);
     }
 }

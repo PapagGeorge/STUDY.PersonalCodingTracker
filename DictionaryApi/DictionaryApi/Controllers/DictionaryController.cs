dc6bf5e0250783525ebbc0cdf5cab224890c1727
ΓΙΩΡΓΟΣ ΠΑΠΑΓΕΩΡΓΙΟΥ <papageorgiou.90@gmail.com>
2024-05-13 00:50:06 +0300

    θπδατε μοδελ

--- DictionaryApi/DictionaryApi/Controllers/DictionaryController.cs
+++ DictionaryApi/DictionaryApi/Controllers/DictionaryController.cs
@@ -20,8 +20,30 @@ namespace DictionaryApi.Controllers
         {
             try
             {
-                var definition = await _urbanDictionaryService.GetDefinitionAsync(term);
-                return Ok(definition);
+                var responseBody = await _urbanDictionaryService.GetDefinitionAsync(term);
+
+                var options = new JsonSerializerOptions()
+                {
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                };
+
+                var definitionList = JsonSerializer.Deserialize<DefinitionList>(responseBody, options);
+                var definitionDtoList = new List<DefinitionDTO>();
+
+                foreach(var item in definitionList.DefinitionModelList)
+                {
+                    var definitionDto = new DefinitionDTO()
+                    {
+                        Author = item.Author,
+                        Definition = item.Definition,
+                        Example = item.Example,
+                        ThumbsUp = item.ThumbsUp,
+                        ThumbsDown = item.ThumbsDown
+                    };
+                    
+                    definitionDtoList.Add(definitionDto);
+                }
+                return Ok(definitionDtoList);
             }
             catch (Exception ex)
             {
--- DictionaryApi/Domain/DefinitionModel.cs
+++ DictionaryApi/Domain/DefinitionModel.cs
@@ -4,25 +4,36 @@ namespace Domain
 {
     public class DefinitionModel
     {
+
         [JsonPropertyName("definition")]
-        public string Definition { get; init; }
+        public string Definition { get; set; }
+
         [JsonPropertyName("permalink")]
-        public string Permalink { get; init; }
+        public string Permalink { get; set; }
+
         [JsonPropertyName("thumbs_up")]
         public int ThumbsUp { get; set; }
+
         [JsonPropertyName("author")]
         public string Author { get; set; }
+
         [JsonPropertyName("word")]
         public string Word { get; set; }
+
         [JsonPropertyName("defid")]
         public long DefId { get; set; }
+
         [JsonPropertyName("current_vote")]
         public string CurrentVote { get; set; }
+
         [JsonPropertyName("written_on")]
         public DateTime WrittenOn { get; set; }
+
         [JsonPropertyName("example")]
         public string Example { get; set; }
+
         [JsonPropertyName("thumbs_down")]
-        public string ThumbsDown { get; set; }
+        public int ThumbsDown { get; set; }
     }
 }
+

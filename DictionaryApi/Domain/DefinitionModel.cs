6c4a8caf2ecee359dcce9cd79e5ebb5764d24933
ΓΙΩΡΓΟΣ ΠΑΠΑΓΕΩΡΓΙΟΥ <papageorgiou.90@gmail.com>
2024-05-12 22:34:28 +0300

    Definition model with JsonPropertyName attributes

---
+++ DictionaryApi/Domain/DefinitionModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Domain
+{
+    public class DefinitionModel
+    {
+        [JsonPropertyName("definition")]
+        public string Definition { get; init; }
+        [JsonPropertyName("permalink")]
+        public string Permalink { get; init; }
+        [JsonPropertyName("thumbs_up")]
+        public int ThumbsUp { get; set; }
+        [JsonPropertyName("author")]
+        public string Author { get; set; }
+        [JsonPropertyName("word")]
+        public string Word { get; set; }
+        [JsonPropertyName("defid")]
+        public long DefId { get; set; }
+        [JsonPropertyName("current_vote")]
+        public string CurrentVote { get; set; }
+        [JsonPropertyName("written_on")]
+        public DateTime WrittenOn { get; set; }
+        [JsonPropertyName("example")]
+        public string Example { get; set; }
+        [JsonPropertyName("thumbs_down")]
+        public string ThumbsDown { get; set; }
+    }
+}

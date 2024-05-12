fc6aa9b92e40f8d2f50a413a40beb0b84a95491a
ΓΙΩΡΓΟΣ ΠΑΠΑΓΕΩΡΓΙΟΥ <papageorgiou.90@gmail.com>
2024-05-12 23:47:52 +0300

    DefinitionDTO and JSON deserialization in controller

--- DictionaryApi/DictionaryApi/Controllers/DictionaryController.cs
+++ DictionaryApi/DictionaryApi/Controllers/DictionaryController.cs
@@ -1,6 +1,8 @@
 ﻿using Application.Interfaces;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using Domain;
+using DictionaryApi.DTOs;
 
 namespace DictionaryApi.Controllers
 {
@@ -20,8 +22,30 @@ namespace DictionaryApi.Controllers
         {
             try
             {
-                var definition = await _urbanDictionaryService.GetDefinitionAsync(term);
-                return Ok(definition);
+                var responseBody = await _urbanDictionaryService.GetDefinitionAsync(term);
+
+                var options = new JsonSerializerOptions()
+                {
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                };
+
+                var definitionList = JsonSerializer.Deserialize<List<DefinitionModel>>(responseBody, options);
+                var definitionDtoList = new List<DefinitionDTO>();
+
+                foreach(var item in definitionList)
+                {
+                    var definitionDto = new DefinitionDTO()
+                    {
+                        Author = item.Author,
+                        Definition = item.Definition,
+                        Example = item.Example,
+                        ThumbsUp = item.ThumbsUp,
+                        ThumbsDown = item.ThumbsDown
+                    };
+                    
+                    definitionDtoList.Add(definitionDto);
+                }
+                return Ok(definitionDtoList);
             }
             catch (Exception ex)
             {
---
+++ DictionaryApi/Domain/DefinitionModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Domain
+{
+    public class DefinitionModel
+    {
+        [JsonPropertyName("definition")]
+        public string Definition { get; init; }
+        [JsonPropertyName("permalink")]
+        public string Permalink { get; init; }
+        [JsonPropertyName("thumbs_up")]
+        public int ThumbsUp { get; init; }
+        [JsonPropertyName("author")]
+        public string Author { get; init; }
+        [JsonPropertyName("word")]
+        public string Word { get; init; }
+        [JsonPropertyName("defid")]
+        public long DefId { get; init; }
+        [JsonPropertyName("current_vote")]
+        public string CurrentVote { get; init; }
+        [JsonPropertyName("written_on")]
+        public DateTime WrittenOn { get; init; }
+        [JsonPropertyName("example")]
+        public string Example { get; init; }
+        [JsonPropertyName("thumbs_down")]
+        public int ThumbsDown { get; init; }
+    }
+}

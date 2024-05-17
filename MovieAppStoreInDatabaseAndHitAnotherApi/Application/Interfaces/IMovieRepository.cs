2d3ae6a90ffbeeafaa08ffe3e63550483c6d7dca
ΓΙΩΡΓΟΣ ΠΑΠΑΓΕΩΡΓΙΟΥ <papageorgiou.90@gmail.com>
2024-05-17 20:48:43 +0300

    Get movie by Id implemented in infrastructure.

--- MovieAppStoreInDatabaseAndHitAnotherApi/Application/Interfaces/IMovieRepository.cs
+++ MovieAppStoreInDatabaseAndHitAnotherApi/Application/Interfaces/IMovieRepository.cs
@@ -5,7 +5,7 @@ namespace Application.Interfaces
     public interface IMovieRepository
     {
         IEnumerable<Movie> GetMoviesByTitle(string title);
-        Movie GetMovieById(int id);
+        Movie GetMovieByImdbId(int id);
 
     }
 }
--- MovieAppStoreInDatabaseAndHitAnotherApi/Infrastructure/Repositories/MovieRepository.cs
+++ MovieAppStoreInDatabaseAndHitAnotherApi/Infrastructure/Repositories/MovieRepository.cs
@@ -1,13 +1,61 @@
 ﻿using Application.Interfaces;
 using Domain.Entities;
+using Infrastructure.Constants;
+using System.Data.SqlClient;
+using System.Data;
+using System.Reflection.Metadata;
 
 namespace Infrastructure.Repositories
 {
-    public class MovieRepository : IMovieRepository
+    public class MovieRepository : BaseRepository, IMovieRepository
     {
-        public Movie GetMovieById(int id)
+        public MovieRepository(DatabaseConfiguration dbConfiguration) : base(dbConfiguration)
         {
-            throw new NotImplementedException();
+            
+        }
+        public Movie GetMovieByImdbId(int id)
+        {
+            using (var connection = GetSqlConnection())
+            {
+                var command = new SqlCommand(StoredProcedures.GetMoviesByImdbId, connection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@ImdbId", id);
+
+                var reader = command.ExecuteReader();
+
+                Movie movie = null;
+                if (reader.Read())
+                {
+                    movie = new Movie
+                    {
+                        Title = reader["Title"].ToString(),
+                        Year = Convert.ToInt32(reader["Year"]),
+                        Rated = reader["Rated"].ToString(),
+                        Released = reader["Released"].ToString(),
+                        Runtime = reader["Runtime"].ToString(),
+                        Genre = reader["Genre"].ToString(),
+                        Director = reader["Director"].ToString(),
+                        Writer = reader["Writer"].ToString(),
+                        Actors = reader["Actors"].ToString(),
+                        Plot = reader["Plot"].ToString(),
+                        Language = reader["Language"].ToString(),
+                        Country = reader["Country"].ToString(),
+                        Awards = reader["Awards"].ToString(),
+                        Poster = reader["Poster"].ToString(),
+                        Metascore = Convert.ToInt32(reader["Metascore"]),
+                        ImdbRating = Convert.ToDouble(reader["ImdbRating"]),
+                        ImdbVotes = Convert.ToInt32(reader["ImdbVotes"]),
+                        ImdbID = reader["ImdbID"].ToString(),
+                        Type = reader["Type"].ToString(),
+                        DVD = reader["DVD"].ToString(),
+                        BoxOffice = reader["BoxOffice"].ToString(),
+                        Production = reader["Production"].ToString(),
+                        Website = reader["Website"].ToString(),
+                        Response = Convert.ToBoolean(reader["Response"])
+                    };
+                }
+                return movie;
+            }
         }
 
         public IEnumerable<Movie> GetMoviesByTitle(string title)

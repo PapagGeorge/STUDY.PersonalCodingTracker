8b757b1e47aec0de1540a267b91148af41984b51
ΓΙΩΡΓΟΣ ΠΑΠΑΓΕΩΡΓΙΟΥ <papageorgiou.90@gmail.com>
2024-05-17 22:37:34 +0300

    Registered Application Services to the DI container.

---
+++ MovieAppStoreInDatabaseAndHitAnotherApi/Infrastructure/InfrastructureServices.cs
@@ -0,0 +1,17 @@
+﻿using Application.Interfaces;
+using Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
+
+namespace Infrastructure
+{
+    public static class InfrastructureServices
+    {
+        public static IServiceCollection InfraServices(this IServiceCollection services)
+        {
+            services.AddScoped<IMovieRepository, MovieRepository>();
+            services.AddScoped<DatabaseConfiguration>();
+            return services;
+        }
+    }
+}

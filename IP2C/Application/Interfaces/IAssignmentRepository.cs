6ea05ef1f8b5979698600cfdc4fe9fe937ab3e63
ΓΙΩΡΓΟΣ ΠΑΠΑΓΕΩΡΓΙΟΥ <papageorgiou.90@gmail.com>
2024-05-29 21:16:37 +0300

    AssignmentRepository - GetCounryByIpAsync

---
+++ IP2C/Application/Interfaces/IAssignmentRepository.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Models;
+
+namespace Application.Interfaces
+{
+    public interface IAssignmentRepository
+    {
+        Task<Country?> GetCountryByIpAsync(string ipAddress);
+        
+    }
+}
---
+++ IP2C/Infrastructure/DatabaseContext/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Domain.Models;
+
+namespace Infrastructure.DatabaseContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<IpAddress> IpAddresses { get; set; }
+    }
+}

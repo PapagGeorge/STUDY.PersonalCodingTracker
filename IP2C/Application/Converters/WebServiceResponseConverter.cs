a88f9ccc15804857992b3eae2a46888cf19160d5
ΓΙΩΡΓΟΣ ΠΑΠΑΓΕΩΡΓΙΟΥ <papageorgiou.90@gmail.com>
2024-05-30 02:47:02 +0300

    Web Service Response Converter

---
+++ IP2C/Application/Converters/WebServiceResponseConverter.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Models;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Application.Converters
+{
+    public class WebServiceResponseConverter : JsonConverter<WebServiceResponse>
+    {
+        public override WebServiceResponse Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            using JsonDocument document = JsonDocument.ParseValue(ref reader);
+
+            string name = document.RootElement.GetProperty("Name").GetString() ?? string.Empty;
+            string twoLetterCode = document.RootElement.GetProperty("TwoLetterCode").ToString() ?? string.Empty;
+            string threeLetterCode = document.RootElement.GetProperty("ThreeLetterCode").ToString() ?? string.Empty;
+
+            return WebServiceResponse.Create(name, twoLetterCode, threeLetterCode);
+        }
+
+        public override void Write(Utf8JsonWriter writer, WebServiceResponse value, JsonSerializerOptions options)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

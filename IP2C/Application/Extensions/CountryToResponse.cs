f4214864c523068c88503532cb4d75f03d711bab
ΓΙΩΡΓΟΣ ΠΑΠΑΓΕΩΡΓΙΟΥ <papageorgiou.90@gmail.com>
2024-05-30 17:29:41 +0300

    Assignment Service

---
+++ IP2C/Application/Extensions/CountryToResponse.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Models;
+
+namespace Application.Extensions
+{
+    public static class CountryToResponse
+    {
+        public static WebServiceResponse TransformCountryToResponse(this Country country)
+        {
+            if (country == null)
+            {
+                throw new ArgumentNullException(nameof(country));
+            }
+
+            return WebServiceResponse.Create(country.Name, country.TwoLetterCode, country.ThreeLetterCode);
+
+            
+        }
+    }
+}
---
+++ IP2C/Infrastructure/UnitOfWorkStructure/UnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using Application.Interfaces;
+using Infrastructure.DatabaseContext;
+
+namespace Infrastructure.UnitOfWorkStructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public IAssignmentRepository AssignmentRepository { get; }
+        private readonly ApplicationDbContext _context;
+        
+
+        public UnitOfWork(IAssignmentRepository assignmentRepository, ApplicationDbContext context)
+        {
+            AssignmentRepository = assignmentRepository ?? throw new ArgumentNullException(nameof(assignmentRepository));
+            _context = context;
+        }
+    }
+    
+}

64385e3602afa683f09496e6263f8557f910b42c
ΓΙΩΡΓΟΣ ΠΑΠΑΓΕΩΡΓΙΟΥ <papageorgiou.90@gmail.com>
2024-06-12 01:34:31 +0300

    Created RedisCache extension methods and called them in the NewsService

--- NewsApiProject/Application/Services/NewsService.cs
+++ NewsApiProject/Application/Services/NewsService.cs
@@ -1,5 +1,7 @@
 ﻿using Application.Interfaces;
 using Domain.Models;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
 
 namespace Application.Services
 {
@@ -7,28 +9,49 @@ namespace Application.Services
     {
         private readonly INewsRepository _newsApiResponseRepository;
         private readonly INewsApiClientRepository _newsApiClient;
+        private readonly IDistributedCache _distributedCache;
 
-        public NewsService(INewsRepository newsApiResponseRepository, INewsApiClientRepository newsApiClient)
+        public NewsService(INewsRepository newsApiResponseRepository,
+            INewsApiClientRepository newsApiClient,
+            IDistributedCache distributedCache)
         {
             _newsApiResponseRepository = newsApiResponseRepository;
             _newsApiClient = newsApiClient;
+            _distributedCache = distributedCache;
         }
         public async Task<NewsApiResponse> GetNewsApiResponse(string keyword)
         {
+            var cacheResponse = await _distributedCache.GetRecordAsync<NewsApiResponse>(keyword, GetJsonSerializerOptions());
+
+            if (cacheResponse != null)
+            {
+                return cacheResponse;
+            }
+
             var newsApiResponse = await _newsApiResponseRepository.GetNewsAsync(keyword);
             if (newsApiResponse != null)
             {
+                await _distributedCache.SetRecordAsync(keyword, newsApiResponse);
                 return newsApiResponse;
             }
 
             newsApiResponse = await _newsApiClient.GetNewsAsync(keyword);
+
             if (newsApiResponse != null)
             {
                 await _newsApiResponseRepository.SetNewsAsync(newsApiResponse);
-                return newsApiResponse;
+                await _distributedCache.SetRecordAsync(keyword, newsApiResponse);
             }
 
-            return default;
+            return newsApiResponse; 
+        }
+
+        private static JsonSerializerOptions GetJsonSerializerOptions()
+        {
+            return new JsonSerializerOptions()
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
         }
     }
 }

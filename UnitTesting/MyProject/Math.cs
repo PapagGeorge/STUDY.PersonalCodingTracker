80c8db0febec582bc8ce7e2696a33a3a984e2ad8
ΓΙΩΡΓΟΣ ΠΑΠΑΓΕΩΡΓΙΟΥ <papageorgiou.90@gmail.com>
2024-06-19 06:25:27 +0300

    Tests for Math class

---
+++ UnitTesting/MyProject/Math.cs
@@ -0,0 +1,15 @@
+﻿namespace MyProject
+{
+    public class Math
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Max(int a, int b)
+        {
+            return (a > b) ? a : b;
+        }
+    }
+}

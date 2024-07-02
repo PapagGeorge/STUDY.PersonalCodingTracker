a45c7379a7e36cad63a6e0143bc5581ad826b867
ΓΙΩΡΓΟΣ ΠΑΠΑΓΕΩΡΓΙΟΥ <papageorgiou.90@gmail.com>
2024-07-02 19:00:28 +0300

    NewsApiModels

---
+++ Assignment_for_NET_Developer_-_Agile_Actors/Domain/Models/NewsApiModels/NewsApiResponse.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Domain.Models.NewsApiModels
+{
+    public class NewsApiResponse
+    {
+        public int NewsApiResponseId { get; init; } // Database primary key, not needed in JSON
+        [JsonPropertyName("status")]
+        public string? Status { get; init; }
+        [JsonPropertyName("totalResults")]
+        public int? TotalResults { get; init; }
+        [JsonPropertyName("articles")]
+        public List<Article> Articles { get; init; }
+    }
+}

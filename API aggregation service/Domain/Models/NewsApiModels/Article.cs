67f77760cfde2b2e70180c2ced9e289099702d65
ΓΙΩΡΓΟΣ ΠΑΠΑΓΕΩΡΓΙΟΥ <papageorgiou.90@gmail.com>
2024-07-02 19:35:46 +0300

    NewsApi Models

---
+++ API aggregation service/Domain/Models/NewsApiModels/Article.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Domain.Models.NewsApiModels
+{
+    public class Article
+    {
+        [JsonPropertyName("author")]
+        public string? Author { get; init; }
+        [JsonPropertyName("title")]
+        public string? Title { get; init; }
+        [JsonPropertyName("description")]
+        public string? Description { get; init; }
+        [JsonPropertyName("url")]
+        public string? Url { get; init; }
+        [JsonPropertyName("urlToImage")]
+        public string? UrlToImage { get; init; }
+        [JsonPropertyName("publishedAt")]
+        public DateTime? PublishedAt { get; init; }
+        [JsonPropertyName("content")]
+        public string? Content { get; init; }
+        // This property maps the nested source object in JSON
+        [JsonPropertyName("source")]
+        public Source Source { get; set; }
+    }
+}

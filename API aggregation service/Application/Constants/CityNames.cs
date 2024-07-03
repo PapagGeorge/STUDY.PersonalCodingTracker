9193c97d12bfb5c04b329422c0b7ff805c28e5ca
ΓΙΩΡΓΟΣ ΠΑΠΑΓΕΩΡΓΙΟΥ <papageorgiou.90@gmail.com>
2024-07-04 00:27:28 +0300

    Sort By Weather

---
+++ API aggregation service/Application/Constants/CityNames.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Constants
+{
+    public class CityNames
+    {
+        public const string Athens = "athens";
+        public const string Thessaloniki = "thessaloniki";
+        //public const string Patra = "patras";
+        //public const string Iraklion = "iraklion";
+        //public const string Larissa = "larissa";
+
+        public static List<string> GetCityNames()
+        {
+            return new List<string> { Athens, Thessaloniki };
+        }
+    }
+}
--- API aggregation service/Domain/Models/AggregateModel/AggregateModel.cs
+++ API aggregation service/Domain/Models/AggregateModel/AggregateModel.cs
@@ -7,7 +7,7 @@ namespace Domain.Models.AggregateModel
     public class AggregateModel
     {
         public NewsApiResponse NewsApiResponse { get; set; }
-        public WeatherData WeatherData { get; set; }
+        public IEnumerable<WeatherData> WeatherData { get; set; }
         public IEnumerable<AstronomyPicture> AstronomyPicture { get; set; }
     }
 }

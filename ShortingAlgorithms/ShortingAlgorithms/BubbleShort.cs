345e57e73042353a9ae3d16be6b00e2622d1d1fe
ΓΙΩΡΓΟΣ ΠΑΠΑΓΕΩΡΓΙΟΥ <papageorgiou.90@gmail.com>
2024-07-20 14:27:17 +0300

    Implemented BubbleShort algorithm

---
+++ ShortingAlgorithms/ShortingAlgorithms/BubbleShort.cs
@@ -0,0 +1,26 @@
+﻿namespace ShortingAlgorithms
+{
+    public class BubbleShort
+    {
+        // Bubble Sort algorithm
+        public static void BubbleShortNumbers(List<int> numbersToShort)
+        {
+            int numbersToShortLength = numbersToShort.Count;
+
+            // Outer loop for each pass
+            for (int i = 0; i < numbersToShortLength - 1; i++)
+            {
+                // Inner loop for comparing adjacent elements
+                for (int j = 0; j < numbersToShortLength - 1 - i; j++)
+                {
+                    if (numbersToShort[j] > numbersToShort[j + 1])
+                    {
+                        int temp = numbersToShort[j];
+                        numbersToShort[j] = numbersToShort[j + 1];
+                        numbersToShort[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+}

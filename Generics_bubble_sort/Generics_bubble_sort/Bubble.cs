2ae3a4c5fbd454884194aadd6075dde26eed0628
ΓΙΩΡΓΟΣ ΠΑΠΑΓΕΩΡΓΙΟΥ <papageorgiou.90@gmail.com>
2024-07-22 18:38:35 +0300

    BubbleGum Sort and swap methods

---
+++ Generics_bubble_sort/Generics_bubble_sort/Bubble.cs
@@ -0,0 +1,29 @@
+﻿namespace Generics_bubble_sort
+{
+    public static class Bubble <T>
+    {
+
+        public static T[] BubbleSortAscending<T>(T[] array) where T : IComparable<T>
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = 0; j < array.Length - i - 1; j++)
+                {
+                    if (array[j].CompareTo(array[j + 1]) > 0)
+                    {
+                        Swap<T>(array, j, j+1);
+                    }
+                }    
+            }
+            
+            return array;
+        }
+
+        private static void Swap<T>(T[] arrayToSwap, int index1, int index2)
+        {
+            T temp = arrayToSwap[index1];
+            arrayToSwap[index1] = arrayToSwap[index2];
+            arrayToSwap[index2] = temp;
+        }
+    }
+}
---
+++ Generics_bubble_sort/Generics_bubble_sort/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Generics_bubble_sort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

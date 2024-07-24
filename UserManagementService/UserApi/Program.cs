19fd682d067660a8ae1f75d9715f68251575831f
ΓΙΩΡΓΟΣ ΠΑΠΑΓΕΩΡΓΙΟΥ <papageorgiou.90@gmail.com>
2024-07-25 00:47:04 +0300

    Created Users Database

--- UserManagementService/UserApi/Program.cs
+++ UserManagementService/UserApi/Program.cs
@@ -1,3 +1,6 @@
+using Infrastructure;
+using Microsoft.Extensions.Configuration;
+
 
 namespace UserApi
 {
@@ -13,6 +16,8 @@ namespace UserApi
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddInfraServices(builder.Configuration);
 
             var app = builder.Build();
 

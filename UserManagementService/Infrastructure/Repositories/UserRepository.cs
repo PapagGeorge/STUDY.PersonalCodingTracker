20416d76dd64ad4fca7b43951a55ab60e77d39d8
ΓΙΩΡΓΟΣ ΠΑΠΑΓΕΩΡΓΙΟΥ <papageorgiou.90@gmail.com>
2024-07-24 23:52:18 +0300

    DbContext and repository registration

---
+++ UserManagementService/Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using Application.Interfaces;
+using Domain;
+
+namespace Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        public async Task AddUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<User> GetUser(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UpdateUser(Guid userId, User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

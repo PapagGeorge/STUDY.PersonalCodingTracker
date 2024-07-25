a6153ebc1056ad78a6c76fa9d1e6f437ebe52870
ΓΙΩΡΓΟΣ ΠΑΠΑΓΕΩΡΓΙΟΥ <papageorgiou.90@gmail.com>
2024-07-25 03:44:24 +0300

    Implemented Default Response in case of errror

---
+++ UserManagementService/Application/ApplicationModule.cs
@@ -0,0 +1,15 @@
+﻿using Application.Interfaces;
+using Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application
+{
+    public static class ApplicationModule
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            services.AddScoped<IUserService, UserService>();
+            return services;
+        }
+    }
+}

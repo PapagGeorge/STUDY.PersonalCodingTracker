8fdafe54899ea9a113d4303b06758ce179bd157b
ΓΙΩΡΓΟΣ ΠΑΠΑΓΕΩΡΓΙΟΥ <papageorgiou.90@gmail.com>
2024-09-21 22:42:30 +0300

    set properties and fields

---
+++ CodeChallenge#1/CodeChallenge#1/Citizen.cs
@@ -0,0 +1,23 @@
+﻿namespace CodeChallenge_1
+{
+    public class Citizen
+    {
+        private int _age;
+
+        public bool CanCitizenVote
+        {
+            get
+            {
+                return _age >= 18 ? true : false;
+            }
+        }
+
+        public int YearsCitizenIsEligibleToVote
+        {
+            get
+            {
+                return _age - 19;
+            }
+        }
+    }
+}
---
+++ CodeChallenge#1/CodeChallenge#1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeChallenge_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

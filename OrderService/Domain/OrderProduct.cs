0b0d1a21bcb2eb35d824193037eea8c825a3945f
ΓΙΩΡΓΟΣ ΠΑΠΑΓΕΩΡΓΙΟΥ <papageorgiou.90@gmail.com>
2024-12-08 16:22:21 +0200

    Set Models for the service

---
+++ OrderService/Domain/OrderProduct.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain
+{
+    public class OrderProduct
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public Guid OrderId { get; set; }
+        public Order Order { get; set; }
+        public Guid ProductId { get; set; }
+        public Product Product { get; set; }
+    }
+}
